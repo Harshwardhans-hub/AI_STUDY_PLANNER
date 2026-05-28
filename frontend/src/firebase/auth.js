fd617ee74f0d2aa998be541e1fedd255550e75f3
Harshwardnan <harshwardhans279sharma@gmail.com>
2026-05-28 14:56:18 +0530

    project pushed

--- frontend/src/firebase/auth.js
+++ frontend/src/firebase/auth.js
@@ -11,13 +11,13 @@ import {
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV82BpABaT5ceaMKDH2wgZaw5iJ4m4WIw",
-  authDomain: "mlh-project-aistudyplanner.firebaseapp.com",
-  projectId: "mlh-project-aistudyplanner",
-  storageBucket: "mlh-project-aistudyplanner.firebasestorage.app",
-  messagingSenderId: "448831905315",
-  appId: "1:448831905315:web:d0f7eb0e26992409371b96",
-  measurementId: "G-ZCPH5CSB2V"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
